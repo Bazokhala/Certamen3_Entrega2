91979314a92c3a8b9d93082dd09e372fcfeb74bb
Bazokhala <lmartin3099@gmail.com>
2022-07-15 14:50:33 -0400

    Agregado PopupMenuButton

--- certamen3_part2/lib/pages/home_page.dart
+++ certamen3_part2/lib/pages/home_page.dart
@@ -1,9 +1,15 @@
+import 'package:certamen3_part2/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +18,25 @@ class HomePage extends StatelessWidget {
         backgroundColor: Color.fromARGB(206, 247, 24, 8),
         leading: Icon(
           MdiIcons.newspaper,),
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context)=>[
+                PopupMenuItem(
+                  value: 'login',
+                  child: Text('Iniciar Sesion'),
+                  onTap: (){
+                    MaterialPageRoute route = MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                      );
+                      Navigator.push(context, route);
+                  },
+                ),
+                PopupMenuItem(
+                  value: 'logout',
+                  child: Text('Cerrar Sesion'))
+              ],
+            )
+          ]
       ),
       body: Text('Noticiones')
     );
